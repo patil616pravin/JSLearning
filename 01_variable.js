6f459f8d736840bbe0bdf690ec65a6030f490406
patil616pravin <patil616pravin@gmail.com>
2023-09-20 21:51:06 +0530

    Designed variable assignment
    Designed variable assignment

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+var firstName; // variable declaration
+firstName = "Pravin";  // Variable initialization
+console.log(firstName);
+firstName = "Mr. Pravin"; // Variable update
+console.log(firstName);
+
+
+var lastName = "Patil"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.2 feet";// update
+console.log(height);
---
+++ 01_variableAssign.js
@@ -0,0 +1,38 @@
+var myFullName;
+myFullName = "Pravin Patil";
+console.log(myFullName);
+myFullName = "Pravin Dilip Patil";
+console.log(myFullName);
+
+var myLove;
+myLove = "I love only JS";
+console.log(myLove);
+
+var firstName;
+firstName = "Pravin";
+console.log(firstName);
+
+var lastName;
+lastName = "Patil";
+console.log(lastName);
+
+var age;
+age = "23";
+console.log(age);
+
+var address;
+address = "Kalyan";
+console.log(address);
+
+var pinCode;
+pinCode = "421102";
+console.log(pinCode);
+
+var gender;
+gender = "male";
+console.log(gender);
+
+var isMarried;
+isMarried = "Unmarried";
+console.log(isMarried);
+
