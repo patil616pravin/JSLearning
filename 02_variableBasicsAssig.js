79129138ee180d88f64329e46e15cc08ad706ee4
patil616pravin <patil616pravin@gmail.com>
2023-09-22 16:24:39 +0530

    updated

--- 02_variableBasicsAssig.js
+++ 02_variableBasicsAssig.js
@@ -1,13 +1,25 @@
 console.log("------------Assignment No.1-------------");
-console.log("--------Step 1--------");
+console.log("-----Step 1-----");
+console.log("--------String Variable--------");
 var FirstName;
+console.log("First Name =>",FirstName);
 FirstName = "Pravin";
-console.log("First Name=",FirstName, typeof FirstName);
+console.log("First Name =>",FirstName,typeof FirstName);
 
-console.log("--------Step 2--------");
+console.log("--------Number Variable--------");
 var PinCode= 421101;
 console.log("PinCode =",PinCode,typeof PinCode);
 
-console.log("--------Step 3-------");
+console.log("--------Boolean Variable-------");
 var IsWorking=true;
 console.log("Working =", IsWorking,typeof IsWorking);
+
+
+console.log("----Step 2------");
+console.log("---------Difference between Undefined & Null---------");
+console.log("******Undefined******");
+var value 
+console.log("Value =>",value);
+console.log("******Null Value******");
+var value
+console.log("Value =>",null);
